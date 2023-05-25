1923fa144dde5f14a217859ab8aedb9afc8dbd7c
Miles Berman <mhberman@ucsc.edu>
2023-05-25 10:43:26 -0700

    Fixed temo ramp down bug

--- app.js
+++ app.js
@@ -10,15 +10,13 @@ const config = {
     }
 };
 
-// A minor
 let chords = [['A4', 'C4', 'E4'], ['F3', 'A4', 'C4'], ['C4', 'E3', 'G3'], ['G3', 'B4', 'D4']];
 let scale = ['A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3','A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4'];
 
-// Song init.
 let melodyIndex = 0;
 let isMelodyPlaying = false;
-let bpm = 110;
-let masterTempo = 110;
+let bpm = 80;
+let masterTempo = 80;
 let loopEvent;
 
 let defaultVolumes = {
@@ -29,9 +27,7 @@ let defaultVolumes = {
     'kick': 0
 };
 
-// Synths
 let synth, hiHat, snare, kick, melodySynth;
-
 let bpmText, synthVolumeText, melodyVolumeText, hiHatVolumeText, snareVolumeText, kickVolumeText;
 
 function preload() {
@@ -39,8 +35,6 @@ function preload() {
 }
 
 function create() {
-
-    // Text fields
     bpmText = this.add.text(10, 10, `BPM: ${bpm}`, { fontSize: '16px', fill: '#000' });
     synthVolumeText = this.add.text(10, 30, '', { fontSize: '16px', fill: '#000' });
     melodyVolumeText = this.add.text(10, 50, '', { fontSize: '16px', fill: '#000' });
@@ -48,16 +42,12 @@ function create() {
     snareVolumeText = this.add.text(10, 90, '', { fontSize: '16px', fill: '#000' });
     kickVolumeText = this.add.text(10, 110, '', { fontSize: '16px', fill: '#000' });
 
-
-    // Chord synth
     synth = new Tone.PolySynth(Tone.Synth).toDestination();
     synth.volume.value = defaultVolumes['synth'];
 
-    // Melody synth
     melodySynth = new Tone.PolySynth(Tone.Synth).toDestination();
     melodySynth.volume.value = defaultVolumes['melodySynth'];
 
-    // hh synth
     hiHat = new Tone.NoiseSynth({
         volume: defaultVolumes['hiHat'],
         envelope: {
@@ -72,7 +62,6 @@ function create() {
         }
     }).toDestination();
 
-    // snare synth
     snare = new Tone.MembraneSynth({
         volume: defaultVolumes['snare'],
         pitchDecay: 0.05,
@@ -89,7 +78,6 @@ function create() {
         }
     }).toDestination();
 
-    // kick synth
     kick = new Tone.MembraneSynth({
         volume: defaultVolumes['kick'],
         pitchDecay: 0.05,
@@ -105,41 +93,41 @@ function create() {
             attackCurve: "exponential"
         }
     }).toDestination();
-    
-    // Start track
+
     let button1 = this.add.sprite(150, 300, 'button').setInteractive();
     button1.on('pointerdown', async function () {
         await Tone.start();
+        Tone.Transport.bpm.value = masterTempo;
         if (!isMelodyPlaying) {
             isMelodyPlaying = true;
             startMelodyLoop();
         }
     });
 
-    // Stop track
     let button2 = this.add.sprite(300, 300, 'button').setInteractive();
     button2.on('pointerdown', async function () {
         if (isMelodyPlaying) {
             isMelodyPlaying = false;
             Tone.Transport.cancel(0);
             Tone.Transport.stop();
+            if (loopEvent) {
+                loopEvent.stop();
+            }
         }
     });
 
-    // Ramp up
     let button3 = this.add.sprite(450, 300, 'button').setInteractive();
     button3.on('pointerdown', function () {
         if(bpm < 200){
-            Tone.Transport.bpm.rampTo(masterTempo + 100, 0.5);
+            Tone.Transport.bpm.rampTo(masterTempo + 200, 2);
             rampDownVolumes();
         }
     });
 
-    // Ramp down
     let button4 = this.add.sprite(600, 300, 'button').setInteractive();
     button4.on('pointerdown', function () {
         if(bpm != 0){
-            Tone.Transport.bpm.rampTo(masterTempo, ); // The second parameter is the ramp time in seconds.
+            Tone.Transport.bpm.rampTo(masterTempo, 2);
             rampUpVolumes();
         }
     });
@@ -151,46 +139,58 @@ function randomMelodyNote() {
 }
 
 function startMelodyLoop() {
-    let noteDuration = 60 / bpm;
-    let halfNoteDuration = noteDuration / 2;
-    loopEvent = Tone.Transport.scheduleRepeat(time => {
+    let loop = new Tone.Loop((time) => {
+        let noteDuration = "4n";
         let chord = chords[melodyIndex % chords.length];
         chord.forEach(note => {
             synth.triggerAttackRelease(note, noteDuration, time);
         });
         melodySynth.triggerAttackRelease(randomMelodyNote(), noteDuration, time);
+
+        // Hi-Hat on every eighth note
         hiHat.triggerAttackRelease("8n", time);
-        hiHat.triggerAttackRelease("8n", time + halfNoteDuration);
-        snare.triggerAttackRelease("8n", time + noteDuration / 2);
-        kick.triggerAttackRelease("8n", time + noteDuration);
+        hiHat.triggerAttackRelease("8n", time + Tone.Time("8n").toSeconds());
+
+        // Kick on the 1 and 3 (every other beat starting from 0)
+        if (melodyIndex % 2 === 0) {
+            kick.triggerAttackRelease(noteDuration, time);
+        }
+
+        // Snare on the 2 and 4 (every other beat starting from 1)
+        else {
+            snare.triggerAttackRelease(noteDuration, time);
+        }
+
         melodyIndex++;
-    }, noteDuration + 's');
+    }, "4n").start(0);
+
     Tone.Transport.start();
 }
 
+
+
 function rampUpVolumes() {
-    synth.volume.rampTo(defaultVolumes['synth'] + 5, 0.5);
-    melodySynth.volume.rampTo(defaultVolumes['melodySynth'] + 5, 0.5);
-    hiHat.volume.rampTo(defaultVolumes['hiHat'] + 5, 0.5);
-    snare.volume.rampTo(defaultVolumes['snare'] + 5, 0.5);
-    //kick.volume.rampTo(defaultVolumes['kick'] + 5, 0.5);
+    synth.volume.rampTo(defaultVolumes['synth'], 3);
+    melodySynth.volume.rampTo(defaultVolumes['melodySynth'], 3);
+    hiHat.volume.rampTo(defaultVolumes['hiHat'], 3);
+    snare.volume.rampTo(defaultVolumes['snare'], 3);
 }
 
 function rampDownVolumes() {
-    synth.volume.rampTo(-Infinity, 2);
-    melodySynth.volume.rampTo(-Infinity, 2);
-    hiHat.volume.rampTo(-Infinity, 2);
-    snare.volume.rampTo(-Infinity, 2);
-   //kick.volume.rampTo(defaultVolumes['kick'], 2);
+    synth.volume.rampTo(-Infinity, 6);
+    melodySynth.volume.rampTo(-Infinity, 6);
+    hiHat.volume.rampTo(-Infinity, 6);
+    snare.volume.rampTo(-Infinity, 6);
 }
 
 function update() {
-    bpmText.setText(`BPM: ${bpm}`);
+    bpm = Tone.Transport.bpm.value;
+    bpmText.setText(`BPM: ${bpm.toFixed(2)}`);
     synthVolumeText.setText(`Synth Volume: ${synth.volume.value.toFixed(2)}`);
     melodyVolumeText.setText(`Melody Volume: ${melodySynth.volume.value.toFixed(2)}`);
-    hiHatVolumeText.setText(`Hi-Hat Volume: ${hiHat.volume.value.toFixed(2)}`);
+    hiHatVolumeText.setText(`HiHat Volume: ${hiHat.volume.value.toFixed(2)}`);
     snareVolumeText.setText(`Snare Volume: ${snare.volume.value.toFixed(2)}`);
     kickVolumeText.setText(`Kick Volume: ${kick.volume.value.toFixed(2)}`);
 }
 
-let game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
